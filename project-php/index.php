dcbf6018aa0fddf1633fc08ddc46b15470bfb508
Denis leibovich <leibovich2021@gmail.com>
2023-02-13 00:43:51 +0200

    project-php

---
+++ project-php/index.php
@@ -0,0 +1,41 @@
+<?
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $file = '/tmp/sample-app.log';
+  $message = file_get_contents('php://input');
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
+}
+else
+{
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>PHP Application - AWS Elastic Beanstalk</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
+    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+</head>
+<body>
+    <section class="congratulations">
+        <h1> lllllllllllllllllllllllllllllllllllllll!</h1>
+ <img src="keshet.jpg"> 
+        <p>yourrrrr thee bestttt<em>PHP</em> application is now running on your own dedicated environment in the 
+    </section>
+ 
+
+
+  
+    </section>
+
+    <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
+</body>
+</html>
+<? 
+} 
+?>
